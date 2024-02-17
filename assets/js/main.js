aa2a05f3fb45461b585fae926601c07b2e9bc8a3
Rominadiazmartinez <Romina.diaz.martinez@gmail.com>
2024-02-16 22:11:43 -0300

    se agrego funcion scrolltop

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,19 @@ $(document).ready(function(){
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+    $(function (){
+
+        var navBar = $("nav");
+
+        $(window).scroll(function(){
+            console.log($(this).scrollTop());
+            if( $(this).scrollTop() >200){
+                navBar.addClass("bg-dark-subtle");
+            }
+            else{
+                navBar.removeClass("bg-dark-subtle");
+            }
+        })
+    })
     
 })
