932074c87afc15589a41e922f9ff6eb386651b21
Rominadiazmartinez <Romina.diaz.martinez@gmail.com>
2024-02-16 22:15:31 -0300

    se agrego dblckick a tarjeta

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,5 +17,35 @@ $(document).ready(function(){
             }
         })
     })
+
+    $("a").on("click", function(event){
+        // console.log("salida de this.hash")
+
+        if(this.hash !== ""){
+            event.preventDefault()
+
+            var hash = this.hash
+
+            $("html, body").animate({
+                scrollTop: $(hash).offset().top
+            }, 2000, function(){                
+            })
+        }
+    })
+
+    var tarjeta = $(".tarjeta");
+
+    function eventPink(){
+        $(this).css({
+            background: "pink"
+        })
+    }
+
+    tarjeta.dblclick(function(){
+        $(this).css({
+            background: "pink",
+            color:"black"
+        })
+    })
     
 })
